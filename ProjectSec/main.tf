72bfc2b765dacbc0fbd9aaaad900db3fe22d24b3
pccrdotnet <pablodotnet@outlook.com>
2022-08-22 18:55:31 -0400

    update main file

--- ProjectSec/main.tf
+++ ProjectSec/main.tf
@@ -1,8 +1,14 @@
 terraform{
-    azurerm = {
-        source = "hashicorp/azurerm"
-        version = "=3.0.0"
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "=3.0.0"
+        }
     }
+}
+
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
